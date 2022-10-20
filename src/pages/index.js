26631540757f9030ef7bdb82e0d0c118bca18cb2
Iodiqy <iodiqy@gmail.com>
2022-10-20 18:30:39 +0600

    initialcommit

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const BlogIndex = ({ data, location }) => {
     return (
       <Layout location={location} title={siteTitle}>
         <Bio />
+	<h1>This is test app</h1>
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
